565dc1f2292ebb46c87d9549731b079f5324a71e
Eiji Kumakawa <morinokumakamesan@gmail.com>
2020-05-02 10:12:36 +0900

    GCA所有者確認追加

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,8 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
+      { name: 'google-site-verification', content: 'yF-BBY_vryvYH9j1tQKLN5VFG9trReTLew13x4cMN-4'}
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
